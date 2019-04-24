5758665a7bfd4d4ab5be163fe34247209c8cb4e7
Jake <js4484@columbia.edu>
2019-04-24 14:32:52 -0400

    finish associations

---
+++ associations/app/models/user.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+
+
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
+    has_many :courses,
+    through: :enrollments,
+    source: :enrolled_courses
+
+    has_many :instructions,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :Course
+
+end
